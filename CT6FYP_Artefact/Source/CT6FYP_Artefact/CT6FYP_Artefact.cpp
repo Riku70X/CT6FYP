7a01d776e13d596c6b7048e2510d19053651afac
Alex <up2060082@myport.ac.uk>
2025-03-03 10:04:11 +0000

    Creating the project

---
+++ CT6FYP_Artefact/Source/CT6FYP_Artefact/CT6FYP_Artefact.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CT6FYP_Artefact.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CT6FYP_Artefact, "CT6FYP_Artefact" );
